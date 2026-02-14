d32aeb01366194efc7431519ff925315b93cb3cc
Amir-630 <1093906@students.adu.ac.ae>
2026-02-15 01:50:42 +0400

    style: standardize quotation marks and formatting in GlobalSearch component

--- src/components/common/GlobalSearch.tsx
+++ src/components/common/GlobalSearch.tsx
@@ -7,28 +7,41 @@ export const GlobalSearch = () => {
       component="form"
       elevation={0}
       sx={{
-        p: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: '40%',
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "40%",
         maxWidth: 800,
-        bgcolor: '#EBE6F3',
+        bgcolor: "#EBE6F3",
         borderRadius: 30,
         height: 38,
-        mx: 'auto' // Center horizontally
+        mx: "auto", // Center horizontally
       }}
     >
-      <M3IconButton sx={{ p: '10px' }} aria-label="search">
+      <M3IconButton sx={{ p: "10px" }} aria-label="search">
         <Search />
       </M3IconButton>
       <M3TextField
-        sx={{ ml: 0, flex: 1 }}
         placeholder="Global Search"
-        inputProps={{ 'aria-label': 'global search' }}
+        inputProps={{ "aria-label": "global search" }}
         variant="outlined"
+        sx={{
+          ml: 1,
+          flex: 1,
+          "& .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+          "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+            border: "none",
+          },
+          "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+            {
+              border: "none",
+            },
+        }}
         size="small"
       />
-      <M3IconButton type="button" sx={{ p: '10px' }} aria-label="options">
+      <M3IconButton type="button" sx={{ p: "10px" }} aria-label="options">
         <MoreVert />
       </M3IconButton>
     </M3Card>
